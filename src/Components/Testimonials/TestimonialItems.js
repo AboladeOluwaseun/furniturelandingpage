4ee2db02e3f7f74a6dec94dc95a0464b9bbb09bb
Maxibrain <aboladeoluwaseungodwithus@gmail.com>
2022-06-29 14:43:30 +0100

    ...

--- src/Components/Testimonials/TestimonialItems.js
+++ src/Components/Testimonials/TestimonialItems.js
@@ -2,34 +2,34 @@ import React from "react";
 import TestimonyCard from "./TestimonyCard";
 import { testimonies } from "./Testimonies";
 
-import Carousel from "react-elastic-carousel";
+// import Carousel from "react-elastic-carousel";
 
 const TestimonialItems = () => {
-  let screenWidth = window.screen.width;
-  console.log(screenWidth);
-  const breakPoints = [
-    { width: 1, itemsToShow: 1 },
-    { width: 600, itemsToShow: 2 },
-    { width: 760, itemsToShow: 3 },
-    { width: 992, itemsToShow: 4 },
-  ];
+  // let screenWidth = window.screen.width;
+  // console.log(screenWidth);
+  // const breakPoints = [
+  //   { width: 1, itemsToShow: 1 },
+  //   { width: 600, itemsToShow: 2 },
+  //   { width: 760, itemsToShow: 3 },
+  //   { width: 992, itemsToShow: 4 },
+  // ];
   const testimoniesDisplay = testimonies.map((testimony) => {
     return (
       <TestimonyCard testimony={testimony} key={testimony.id}></TestimonyCard>
     );
   });
 
-  const testimoniesDisplayItems = () => {
-    if (screenWidth <= 360) {
-      return <div>{testimoniesDisplay}</div>;
-    } else
-      return (
-        <Carousel breakPoints={breakPoints} enableAutoPlay autoPlaySpeed={1500}>
-          {testimoniesDisplay}
-        </Carousel>
-      );
-  };
-  return <>{testimoniesDisplayItems()}</>;
+  // const testimoniesDisplayItems = () => {
+  //   if (screenWidth <= 360) {
+  //     return <div>{testimoniesDisplay}</div>;
+  //   } else
+  //     return (
+  //       <Carousel breakPoints={breakPoints} enableAutoPlay autoPlaySpeed={1500}>
+  //         {testimoniesDisplay}
+  //       </Carousel>
+  //     );
+  // };
+  return <>{testimoniesDisplay}</>;
 };
 
 export default TestimonialItems;
