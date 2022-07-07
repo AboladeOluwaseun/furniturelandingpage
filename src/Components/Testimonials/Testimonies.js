958ddb51e4ea588d6dfc4b9d37d3b52b03ab73ed
Maxibrain <aboladeoluwaseungodwithus@gmail.com>
2022-07-07 15:21:06 +0100

    ...

--- src/Components/Testimonials/Testimonies.js
+++ src/Components/Testimonials/Testimonies.js
@@ -34,4 +34,74 @@ export const testimonies = [
       "My sofa has been my best purchase so far. Came with the best of customer service and has had no issues since purchase. ",
     id: "5",
   },
+  {
+    name: "Temi Makanjuola",
+    location: "Lagos, Nigeria",
+    testimony:
+      "My sofa has been my best purchase so far. Came with the best of customer service and has had no issues since purchase. ",
+    id: "1",
+  },
+  {
+    name: "Temi Makanjuola",
+    location: "Lagos, Nigeria",
+    testimony:
+      "My sofa has been my best purchase so far. Came with the best of customer service and has had no issues since purchase. ",
+    id: "2",
+  },
+  {
+    name: "Temi Makanjuola",
+    location: "Lagos, Nigeria",
+    testimony:
+      "My sofa has been my best purchase so far. Came with the best of customer service and has had no issues since purchase. ",
+    id: "3",
+  },
+  {
+    name: "Temi Makanjuola",
+    location: "Lagos, Nigeria",
+    testimony:
+      "My sofa has been my best purchase so far. Came with the best of customer service and has had no issues since purchase. ",
+    id: "4",
+  },
+  {
+    name: "Temi Makanjuola",
+    location: "Lagos, Nigeria",
+    testimony:
+      "My sofa has been my best purchase so far. Came with the best of customer service and has had no issues since purchase. ",
+    id: "5",
+  },
+  {
+    name: "Temi Makanjuola",
+    location: "Lagos, Nigeria",
+    testimony:
+      "My sofa has been my best purchase so far. Came with the best of customer service and has had no issues since purchase. ",
+    id: "1",
+  },
+  {
+    name: "Temi Makanjuola",
+    location: "Lagos, Nigeria",
+    testimony:
+      "My sofa has been my best purchase so far. Came with the best of customer service and has had no issues since purchase. ",
+    id: "2",
+  },
+  {
+    name: "Temi Makanjuola",
+    location: "Lagos, Nigeria",
+    testimony:
+      "My sofa has been my best purchase so far. Came with the best of customer service and has had no issues since purchase. ",
+    id: "3",
+  },
+  {
+    name: "Temi Makanjuola",
+    location: "Lagos, Nigeria",
+    testimony:
+      "My sofa has been my best purchase so far. Came with the best of customer service and has had no issues since purchase. ",
+    id: "4",
+  },
+  {
+    name: "Temi Makanjuola",
+    location: "Lagos, Nigeria",
+    testimony:
+      "My sofa has been my best purchase so far. Came with the best of customer service and has had no issues since purchase. ",
+    id: "5",
+  },
 ];
